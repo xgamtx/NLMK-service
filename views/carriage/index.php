7cc9a08173e8f6146467244010a9bc66b59d3117
Vasiliy <zaluev@lightsoft.ru>
2015-02-17 22:19:08 +0300

    12: Добавил кнопки

--- views/carriage/index.php
+++ views/carriage/index.php
@@ -3,7 +3,6 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 use app\models\Carriage;
-use yii\bootstrap\Button;
 
 /* @var $this yii\web\View */
 /* @var $searchModel app\models\CarriageSearch */
@@ -17,7 +16,39 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <p><a href="#modal" role="button" class="btn btn-primary" data-toggle="modal">Загрузить</a></p>
+    <div class="control-panel">
+        <div class="dropdown control-button">
+            <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown"
+                    aria-expanded="true">
+                Действия
+                <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Печать</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Сохранить</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Прибыл</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Утвержден на демонтаж</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Демонтирован</a></li>
+                <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Архив</a></li>
+            </ul>
+        </div>
+        <a href="#modal" role="button" class="btn btn-primary control-button">Склад</a>
+        <a href="#modal" role="button" class="btn btn-primary control-button">Архив</a>
+        <a href="#modal" role="button" class="btn btn-primary control-button" data-toggle="modal">Добавить</a>
+        <div class="gluh"></div>
+        <div class="input-group control-input"><input type="text" class="form-control" placeholder="Номер вагона"></div>
+        <div class="input-group control-input"><input type="text" class="form-control" placeholder="Тип вагона"></div>
+        <div class="input-group control-input"><input type="text" class="form-control" placeholder="ПЗУ"></div>
+        <a href="#modal" role="button" class="btn btn-primary control-button">М1</a>
+        <a href="#modal" role="button" class="btn btn-primary control-button">М2</a>
+        <a href="#modal" role="button" class="btn btn-primary control-button">М3</a>
+        <a href="#modal" role="button" class="btn btn-primary control-button">Статус</a>
+        <div class="input-group control-input"><input type="text" class="form-control" placeholder="Склад"></div>
+        <div class="input-group control-input"><input type="text" class="form-control" placeholder=""></div>
+        <span class="defis">-</span>
+        <div class="input-group control-input"><input type="text" class="form-control" placeholder=""></div>
+        <div class="gluh"></div>
+    </div>
 
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
