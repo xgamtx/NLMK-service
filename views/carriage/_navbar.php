676db01fc0f35d6056e7318ba1c204cdb7f1e808
vasilij <klunew@yandex.ru>
2015-04-16 22:33:41 +0300

    43: заблокировал вкладки

--- views/carriage/_navbar.php
+++ views/carriage/_navbar.php
@@ -16,15 +16,31 @@ use yii\helpers\Html;
         <?= Html::a('Опись вагона', ['carriage/inventory', 'id' => $model->id]);?>
     </li>
     <li role="presentation" class="<?= $selectTabId == 2 ? 'active' : ''?>">
+        <? if ($model->isFullInfoEnabled()): ?>
         <?= Html::a('Заявка на демонтаж', ['carriage/disassembling-order', 'id' => $model->id]);?>
+        <? else: ?>
+            <?= Html::a('Заявка на демонтаж');?>
+        <? endif; ?>
     </li>
     <li role="presentation" class="<?= $selectTabId == 3 ? 'active' : ''?>">
+        <? if ($model->isFullInfoEnabled()): ?>
         <?= Html::a('Акт передачи вагона на демонтаж', ['carriage/disassembling-send', 'id' => $model->id]);?>
+        <? else: ?>
+            <?= Html::a('Акт передачи вагона на демонтаж');?>
+        <? endif; ?>
     </li>
     <li role="presentation" class="<?= $selectTabId == 4 ? 'active' : ''?>">
+        <? if ($model->isFullInfoEnabled()): ?>
         <?= Html::a('МХ-1', ['carriage/mh1', 'id' => $model->id]);?>
+        <? else: ?>
+            <?= Html::a('МХ-1');?>
+        <? endif; ?>
     </li>
     <li role="presentation" class="<?= $selectTabId == 5 ? 'active' : ''?>">
-        <?= Html::a('Опись МЦ	', ['carriage/inventory-m-c', 'id' => $model->id]);?>
+        <? if ($model->isFullInfoEnabled()): ?>
+        <?= Html::a('Опись МЦ', ['carriage/inventory-m-c', 'id' => $model->id]);?>
+        <? else: ?>
+            <?= Html::a('Опись МЦ');?>
+        <? endif; ?>
     </li>
 </ul>
